896c5a7284c78f3c61cde2441e999385199e2112
Vinicius Silva de Oliveira <2025107290023@aluno.ifro.edu.br>
2025-12-01 22:20:35 -0400

    atualização codigo

---
+++ piton/Listas.py
@@ -0,0 +1,50 @@
+##1
+Lista=[]
+for x in range(5):
+    valor=int(input("Digite um valor inteiro: "))
+    Lista.append(valor)
+    
+print(f"Os elementos da lista são: {Lista}")
+
+#mostra os valores dentro da lista pelo indice
+for x in range(5):
+    print(Lista[x])
+    #exemplo 2    
+for x in Lista:
+    print(x) 
+    
+    
+##2
+Lista=[]
+for x in range(12):
+    valor=int(input("Digite um valor inteiro: "))
+    Lista.append(valor)
+print(f"O 7 elemento da lista é: {Lista[6]}")
+
+##3
+Lista=[]
+for x in range(12):
+    valor=int(input("Digite um valor inteiro: "))
+    Lista.append(valor)
+print(f"O 7 elemento da lista é: {Lista[4]}")
+
+##4
+Lista=[]
+qtd=int(input("Informe quantos valores deseja digitar:  "))
+for x in range(qtd):
+    valor=int(input("Digite um valor inteiro: "))
+    Lista.append(valor)
+print(f"O maior valor da lista é {max(Lista)}, e menor é {min(Lista)}.")
+
+##5
+Lista[]
+qtd=int(input("Informe quantos valores deseja digitar:  "))
+for x in range(qtd):
+    valor=int(input("Digite um valor inteiro: "))
+    Lista.append(valor)
+    
+print(f"Os valores da lista são{Lista}")
+print(f"O somatório dos valores da lista é = {sum(Lista)}")
+
+##6
+listaM=[]
